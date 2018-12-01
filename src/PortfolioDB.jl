98b19e26bda3eba6137376bb0eaecf293596c829
ewybenga <e.wybenga@gmail.com>
2018-12-01 14:21:05 -0500

    created PortfolioDB struct

--- src/PortfolioDB.jl
+++ src/PortfolioDB.jl
@@ -1,12 +1,25 @@
-module PortfolioDBMod
+include("./MarketData.jl")
+include("./Portfolios.jl")
 
-	"""
-		PortfolioDB()
+export PortfolioDB
 
-	The 
-	"""
+"""
+	PortfolioDB()
+
+The
+"""
 
 
+function write(date::Date, portfolio::Portfolio, volatility::Float64, riskreward::Float64)
+    println(date)
+end
 
+mutable struct PortfolioDB
+    data::DataFrame
+    function PortfolioDB()
+        # initialize a new DataFrame to hold temporal portfolio data
+        data = DataFrame(date=Date[], value=Float64[], capital=Float64[], holdings=Dict[], volatility=Float64[], riskreward=Float64[])
 
+        return new(data)
+    end
 end
