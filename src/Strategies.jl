40d597f137ce2b42c5982fb127f86579562ccd8b
= <kimholmgren@cs.uchicago.edu>
2018-12-03 18:57:57 -0500

    added otherdata to strategy

--- src/Strategies.jl
+++ src/Strategies.jl
@@ -3,7 +3,7 @@ include("./Tickers.jl")
 include("./PortfolioDB.jl")
 
 """
-    Strategy(processInfo, portfolio)
+    Strategy(name, processInfo, portfolio, mdb)
 
 This is the structure for a Strategy object. It must have a function that dictates what action(s) to take on a portfolio when a new day of information is available. The processInfo function must be designed such that it takes in the following arguments:
     processInfo(marketData::MarketDB, date::Date, portfolioData::PortfolioDB, portfolio::Portfolio, transfee::Float64)
@@ -12,10 +12,12 @@ mutable struct Strategy
     name::String
     processInfo::Function
     portfolio::Portfolio
+    otherData::Dict
     pdb::PortfolioDB
     function Strategy(name::String, processInfo::Function, portfolio::Portfolio, mdb::MarketDB)
         pdb = PortfolioDB(mdb)
-        return new(name, processInfo, portfolio, pdb)
+        otherData = Dict()
+        return new(name, processInfo, portfolio, otherData, pdb)
     end
 
 end
