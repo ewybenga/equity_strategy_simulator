bc1788ae0c3f4ee8c85d336202f62bc3c06bed60
Eric Wybenga <e.wybenga@gmail.com>
2018-12-04 16:35:08 -0500

    added buyDropSellGain function to strategies

--- src/Strategies.jl
+++ src/Strategies.jl
@@ -57,6 +57,56 @@ function Example2(marketData, date, portfolioData, portfolio, transfee, otherDat
 end
 
 """
+    buyDropSellGain()
+
+defines a strategy that buys all the google it has the cash for if the sum of daily returns for the last 5 days is less than -5% and sells off half of the number of shares of google it has if the sum of daily returns for the last 5 days is greater than 5%
+"""
+function buyDropSellGain(marketData, date, portfolioData, portfolio, transfee, otherData::Dict)
+    function computeDailyReturnPerShare(portfolio::Portfolio, date::Date, mdb::MarketDB, ticker::Ticker)
+        if date <= mdb.data[:date][1]
+            return 0
+        end
+        # find ticker trade price on the last trading day
+        p_yesterday = getMostRecentPrice(mdb, date-Day(1), ticker)
+        # find current price
+        p_current = getMostRecentPrice(mdb, date, ticker)
+        # compute annualized return
+        return (p_current/p_yesterday) - 1.
+    end
+
+    goog = Ticker(marketData, "Q", "GOOG")
+
+    returns = Float64[]
+    for date in date-Day(5):Day(1):date
+        append!(returns, computeDailyReturnPerShare(p, date, m, goog))
+    end
+    sumval = 0.
+    sumval = sum(returns)
+    if sumval <= -0.05
+        price = queryMarketDB(marketData, date, goog, :prc)
+        if ismissing(price)
+            return
+        else
+            numShares = floor(portfolio.capital/price[1].value)
+            buy(portfolio, goog, numShares, date, transfee, marketData)
+        end
+    elseif sumval >= 0.05
+        price = queryMarketDB(marketData, date, goog, :prc)
+        if ismissing(price)
+            return
+        else
+            if length(portfolio.holdings) == 0
+                return
+            else
+            sell(portfolio, goog, floor(portfolio.holdings[goog]/2.0), date, transfee, marketData)
+            end
+        end
+    end
+
+    return
+end
+
+"""
     SandPBaseline()
 
 Defines a strategy to hold the S&P 500 and buy it if it has cash
